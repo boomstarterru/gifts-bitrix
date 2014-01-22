20aa0cc822cd770af1d3ae7119f3e7175caeb7d7
Vitaly.Fadeev <vital.fadeev@gmail.com>
2014-01-22 16:23:02 +0800

    * update general class

--- boomstarter.gifts/classes/general/boomstarter_gifts.php
+++ boomstarter.gifts/classes/general/boomstarter_gifts.php
@@ -8,7 +8,8 @@
 
 namespace classes\general;
 
-// CModule::IncludeModule("sale");
+CModule::IncludeModule("sale");
+CModule::IncludeModule("catalog");
 
 
 class boomstarter_gifts {
@@ -41,28 +42,73 @@ class boomstarter_gifts {
         $gifts = $api->getGiftsPending();
 
         foreach($gifts as $gift) {
-            // create order
-            $order_id = CSaleOrder::Add(array(
-                    // "LID" => "ru",
-                    "PERSON_TYPE_ID" => 1,
-                    "PAYED" => "N",
-                    "CANCELED" => "N",
-                    "STATUS_ID" => "N",
-                    // "PRICE" => 279.32,
-                    // "CURRENCY" => "USD",
-                    // "USER_ID" => IntVal($USER->GetID()),
-                    // "PAY_SYSTEM_ID" => 3,
-                    // "PRICE_DELIVERY" => 11.37,
-                    // "DELIVERY_ID" => 2,
-                    // "DISCOUNT_VALUE" => 1.5,
-                    // "TAX_VALUE" => 0.0,
-                    "USER_DESCRIPTION" => "Подарок через Boomstarter Gifts API",
-                    'BOOMSTARTER_GIFT_UUID' => $gift->uuid
-                ));
-            $order_id = IntVal($order_id)
+            $product_id = $gift->product_id;
 
-            // send order_id
+            $product = $this->getProduct($product_id);
+
+            $price = $product['PURCHASING_PRICE'];
+            $currency = $product['PURCHASING_CURRENCY'];
+            $product_name = $product['NAME']; // ?
+
+            $order_id = $this->createOrder($price, $currency, $gift->uuid);
+
+            $this->addToBasket($product_id, $product_name, $price, $currency);
+
+            CSaleBasket::DeleteAll(CSaleBasket::GetBasketUserID(), False);
+
+            Add2BasketByProductID($PRODUCT_ID = $name_and_price[$i]["id"], $QUANTITY = 1, true);
+
             $gift->order($order_id);
         }
+    }
+
+    private function createOrder($price, $currency, $gift_uuid)
+    {
+        $order_id = CSaleOrder::Add(array(
+                // "LID" => "ru", // LANG
+                "PERSON_TYPE_ID" => 1,
+                "PAYED" => "N",
+                "CANCELED" => "N",
+                "STATUS_ID" => "N",
+                "PRICE" => $price,
+                "CURRENCY" => $currency,
+                // "USER_ID" => IntVal($USER->GetID()),
+                // "PAY_SYSTEM_ID" => 3,
+                "PRICE_DELIVERY" => 0,
+                // "DELIVERY_ID" => 2,
+                "DISCOUNT_VALUE" => 0,
+                "TAX_VALUE" => 0.0,
+                "USER_DESCRIPTION" => "Подарок через Boomstarter Gifts API",
+                'BOOMSTARTER_GIFT_UUID' => $gift_uuid
+            ));
+        $order_id = IntVal($order_id)
+    }
+
+    private function getProduct($product_id)
+    {
+        $ar_res = CCatalogProduct::GetByID($product_id);
+        return $ar_res;
+    }
+
+    private function addToBasket($product_id, $product_name, $price, $currency)
+    {
+        $site = ''; // ? LID - сайт, на котором сделана покупка (обязательное поле);
+
+        $arFields = array(
+            "PRODUCT_ID" => $product_id,
+            "PRODUCT_PRICE_ID" => 0,
+            "PRICE" => $price,
+            "CURRENCY" => $currency,
+            "QUANTITY" => 1,
+            "LID" => $site,
+            "DELAY" => "Y",
+            "CAN_BUY" => "Y",
+            "NAME" => $product_name,
+            "MODULE" => $this->MODULE_ID,
+            "NOTES" => "Подарок через Boomstarter Gifts API",
+            // "DETAIL_PAGE_URL" => "/".LANG."/detail.php?ID=".$product_id
+        );
+
+        CSaleBasket::Add($arFields);
     }
 }
