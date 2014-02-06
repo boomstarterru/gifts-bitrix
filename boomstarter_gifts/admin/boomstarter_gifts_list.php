70431ea71ab3a6cf263b627ed3f350ee313d14ad
Vitaly.Fadeev <vital.fadeev@gmail.com>
2014-02-06 15:34:55 +0800

    * скрипт админки в папку Bitrix

---
+++ boomstarter_gifts/admin/boomstarter_gifts_list.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_after.php");
+
+if (!\CModule::IncludeModule('boomstarter_gifts')) {
+    die('{"error":"Module \"boomstarter_gifts\" not installed"}');
+}
+
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/boomstarter_gifts/include.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/boomstarter_gifts/classes/general/ControllerBitrix.php");
+
+$admin = new \Boomstarter\Gifts\ControllerBitrix();
+$admin->run();
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_admin.php");
