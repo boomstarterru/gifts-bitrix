521a779fb6a212bdd4d92619c3acb03fb66572ff
Vitaly.Fadeev <vital.fadeev@gmail.com>
2014-03-31 15:24:28 +0800

    * 2Вернуться в списо" в инсталяторе

--- boomstarter_gifts/install/step.php
+++ boomstarter_gifts/install/step.php
@@ -7,3 +7,10 @@ IncludeModuleLangFile($_SERVER['DOCUMENT_ROOT'].BX_ROOT.'/modules/boomstarter_gi
 
 echo CAdminMessage::ShowNote(GetMessage('INSTALL_MODULE_SUCCESS'));
 
+?>
+<div style="font-size: 12px;"></div>
+<br>
+<form action="<?echo $APPLICATION->GetCurPage()?>">
+    <input type="hidden" name="lang" value="<?echo LANG?>">
+    <input type="submit" name="" value="<?echo GetMessage("MOD_BACK")?>">
+</form>
--- boomstarter_gifts/install/unstep.php
+++ boomstarter_gifts/install/unstep.php
@@ -6,3 +6,9 @@ if (!check_bitrix_sessid())
 IncludeModuleLangFile($_SERVER['DOCUMENT_ROOT'].BX_ROOT.'/modules/boomstarter_gifts/install/unstep.php', 'ru.'.LANG_CHARSET);
 
 echo CAdminMessage::ShowNote(GetMessage('UNINSTALL_MODULE_SUCCESS'));
+?>
+<br>
+<form action="<?echo $APPLICATION->GetCurPage()?>">
+    <input type="hidden" name="lang" value="<?echo LANG?>">
+    <input type="submit" name="" value="<?echo GetMessage("MOD_BACK")?>">
+</form>
