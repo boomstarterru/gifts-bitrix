16cd7b7ea305a23a0c5f16ccc3b92fcdbd13fe11
Vitaly.Fadeev <vital.fadeev@gmail.com>
2014-03-31 13:46:02 +0800

    lang

---
+++ boomstarter_gifts/lang/ru.CP1251/classes/general/ControllerBitrix.php
@@ -0,0 +1,9 @@
+<?php
+$MESS['LISI_OF_PENDING_GIFTS'] = "Список оплаченных подарков";
+$MESS['HEADER_IMAGE'] = "Фото";
+$MESS['HEADER_NAME'] = "Нименование";
+$MESS['HEADER_PRICE'] = "Цена";
+$MESS['HEADER_PRODUCT_ID'] = "PRODUCT_ID";
+$MESS['HEADER_ORDER'] = "Заказ";
+$MESS['HEADER_STATUS'] = "Статус";
+$MESS['HEADER_ACTION'] = "";
